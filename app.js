08fa725cc632f6dc1333a55f732762f4651062c8
cmplasistema <cmpla.sistema@gmail.com>
2021-06-10 16:50:23 -0300

    .

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+const { Sequelize, DataTypes } = require('sequelize');
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('melob187_cmp', 'melob187_cmpu', '7B6h&c3mu', {
+    host: 'br882.hostgator.com.br',
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 5000
+      },
+  });
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
+var Clientes = sequelize.define('clientes', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING
+    },
+    endereco: {
+        type: DataTypes.STRING
+    },
+    cidade: {
+        type: DataTypes.STRING
+    },
+    telefone: {
+        type: DataTypes.STRING
     }
-});
+})
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+//Cria colunas caso não exista no banco de dados
+// Clientes.sync({ force: true });         //// sequelize.sync({ force: true }); sincroniza todas as talelas de uma só vez
+// console.log("The table for the Clientes model was just (re)created!");
 
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+async function main() {
+    const jane = Clientes.build({ nome: "Jack" });
+    console.log(jane instanceof Clientes); // true
+    console.log(jane.nome); // "Jane"
+    
+    await jane.save();
+    console.log('Jane was saved to the database!');
+  }
+
+main();
