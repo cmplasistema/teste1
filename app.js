548493d19c8a12ab1f76428c2beecb06d0a2e08e
cmplasistema <cmpla.sistema@gmail.com>
2021-06-10 17:25:45 -0300

    .

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const { Sequelize, DataTypes } = require('sequelize');
+var AWS = require("aws-sdk");
 
+AWS.config.update({
+  region: "us-east-2",
+  endpoint: "http://localhost:8000"
+});
 
-// Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('melob187_cmp', 'melob187_cmpu', '7B6h&c3mu', { });
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "Movies",
+    KeySchema: [       
+        { AttributeName: "year", KeyType: "HASH"},  //Partition key
+        { AttributeName: "title", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "year", AttributeType: "N" },
+        { AttributeName: "title", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
 
 
 var port = process.env.PORT || 3000,
